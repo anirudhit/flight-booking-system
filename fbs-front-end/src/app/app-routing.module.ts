f7c57ce1ee61cb5a216b3f5be1b022c6219c9ede
anirudhit <anirudhit@outlook.com>
2018-10-05 13:33:33 -0500

    - Added files for front end and back end

---
+++ fbs-front-end/src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+
+const routes: Routes = [
+  { 
+     path: 'flights', 
+     component: FlightSearchComponent 
+   },
+   { 
+     path: 'customers', 
+     component: CustomerComponent 
+   },
+   { 
+     path: 'customer/add', 
+     component: AddCustomerComponent 
+   },
+   { 
+     path: 'customers/:id', 
+     component: CustomerDetailsComponent 
+   },
+   { 
+     path: '', 
+     redirectTo: 'customers', 
+     pathMatch: 'full'
+   }, 
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
---
+++ fbs-front-end/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material';
+
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    CustomerDetailsComponent,
+    AddCustomerComponent,
+    FlightSearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
