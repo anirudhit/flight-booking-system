a5f57fffffceb05c183f3bffde4816a12dc59dab
anirudhit <anirudhit@outlook.com>
2018-10-05 15:57:40 -0500

    Backend
    - Added connection string for aws my sql instance

--- fbs-back-end/app/config/env.js
+++ fbs-back-end/app/config/env.js
@@ -1,8 +1,14 @@
 const env = {
+  //Localhost instance mapping
   database: 'testdb',
   username: 'root',
   password: 'uah@1208',
   host: 'localhost',
+  //AWS instance mapping
+  // database: 'flightbookingsystem',
+  // username: 'adminfbs',
+  // password: 'uah#1500',
+  // host: 'fbs.cegaauo5au7e.us-east-1.rds.amazonaws.com',
   dialect: 'mysql',
   pool: {
 	  max: 5,
