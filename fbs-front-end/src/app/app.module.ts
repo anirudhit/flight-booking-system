a0ced124a9ecfd2a1ede221db2343705ca51f566
anirudhit <anirudhit@outlook.com>
2018-10-06 19:10:21 -0500

    app module
    - Updated app module for the login page

--- fbs-front-end/src/app/app.module.ts
+++ fbs-front-end/src/app/app.module.ts
@@ -1,37 +1,41 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+
 import { HttpClientModule }    from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AppMaterialModule } from './app-material/app-material.module';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthService } from './auth/auth.service';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material';
-
-import { CustomerComponent } from './customer/customer.component';
-import { CustomerDetailsComponent } from './customer-details/customer-details.component';
-import { AddCustomerComponent } from './add-customer/add-customer.component';
-import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+
+// import { CustomerComponent } from './customer/customer.component';
+// import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+// import { AddCustomerComponent } from './add-customer/add-customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent,
-    CustomerDetailsComponent,
-    AddCustomerComponent,
-    FlightSearchComponent,
-    LoginComponent
+    HomeComponent,
+    HeaderComponent,
+    LoginComponent//,
+    // CustomerComponent,
+    // CustomerDetailsComponent,
+    // AddCustomerComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    MaterialModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
