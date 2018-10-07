74254373ad4444b148e905cb55cf07852168d436
anirudhit <anirudhit@outlook.com>
2018-10-06 19:09:34 -0500

    app routing
    - Updated app routing for the login page

--- fbs-front-end/src/app/app-routing.module.ts
+++ fbs-front-end/src/app/app-routing.module.ts
@@ -1,37 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './login/login.component';
-import { CustomerComponent } from './customer/customer.component';
-import { AddCustomerComponent } from './add-customer/add-customer.component';
-import { CustomerDetailsComponent } from './customer-details/customer-details.component';
-import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { HomeComponent } from './home/home.component';
+
+// import { CustomerComponent } from './customer/customer.component';
+// import { AddCustomerComponent } from './add-customer/add-customer.component';
+// import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 
 const routes: Routes = [
-  { 
-    path: 'login', 
-    component: LoginComponent 
-  },
-  { 
-     path: 'flights', 
-     component: FlightSearchComponent 
-   },
-   { 
-     path: 'customers', 
-     component: CustomerComponent 
-   },
-   { 
-     path: 'customer/add', 
-     component: AddCustomerComponent 
-   },
-   { 
-     path: 'customers/:id', 
-     component: CustomerDetailsComponent 
-   },
-   { 
-     path: '', 
-     redirectTo: 'login', 
-     pathMatch: 'full'
-   }, 
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  // { path: '**', redirectTo: ''},
+  // { 
+  //   path: 'customers', 
+  //   component: CustomerComponent 
+  // },
+  // { 
+  //   path: 'customer/add', 
+  //   component: AddCustomerComponent 
+  // },
+  // { 
+  //   path: 'customers/:id', 
+  //   component: CustomerDetailsComponent 
+  // }
 ];
 
 @NgModule({
