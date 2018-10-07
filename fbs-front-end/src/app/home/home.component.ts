9ac0ac1e2adb659545449f5e368081e05cf21182
anirudhit <anirudhit@outlook.com>
2018-10-06 19:00:24 -0500

    home component
    - Updated the login message

--- fbs-front-end/src/app/home/home.component.ts
+++ fbs-front-end/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template:'<p>Yay! You are logged in!</p>',
+  template:'<p>You are logged in!</p>',
   styles: []
 })
 export class HomeComponent implements OnInit {
