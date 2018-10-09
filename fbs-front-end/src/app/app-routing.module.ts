fe0d06a770541e3d93896ebbffec2c129c80a97a
anirudhit <anirudhit@outlook.com>
2018-10-08 22:51:19 -0500

    app routung module (FE)
    - Injected register component

--- fbs-front-end/src/app/app-routing.module.ts
+++ fbs-front-end/src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './login/components/login.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 // import { CustomerComponent } from './customer/customer.component';
 // import { AddCustomerComponent } from './add-customer/add-customer.component';
 // import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'register', component: RegisterComponent }
   // { path: '**', redirectTo: ''},
   // { 
   //   path: 'customers', 
