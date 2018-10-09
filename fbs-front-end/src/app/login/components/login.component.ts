fb3c34a103580476569403622b7f14cc7e1439ed
anirudhit <anirudhit@outlook.com>
2018-10-09 12:22:12 -0500

    login component (FE)
    - Updated FormGroup attribute
    - Moved user card styles to global styles

--- fbs-front-end/src/app/login/components/login.component.ts
+++ fbs-front-end/src/app/login/components/login.component.ts
@@ -9,7 +9,7 @@ import { LoginUser } from './../models/loginUser';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  form: FormGroup;                    // {1}
+  userLoginForm: FormGroup;                    // {1}
   private formSubmitAttempt: boolean; // {2}
 
   constructor(
@@ -19,7 +19,7 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.form = this.fb.group({     // {5}
+    this.userLoginForm = this.fb.group({     // {5}
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
@@ -27,21 +27,21 @@ export class LoginComponent implements OnInit {
 
   isFieldInvalid(field: string) { // {6}
     return (
-      (!this.form.get(field).valid && this.form.get(field).touched) ||
-      (this.form.get(field).untouched && this.formSubmitAttempt)
+      (!this.userLoginForm.get(field).valid && this.userLoginForm.get(field).touched) ||
+      (this.userLoginForm.get(field).untouched && this.formSubmitAttempt)
     );
   }
 
   onSubmit() {
-    if (this.form.valid) {
+    if (this.userLoginForm.valid) {
       let loginUser = new LoginUser();
-      loginUser.name = this.form.value.userName;
-      loginUser.password = this.form.value.password;
+      loginUser.name = this.userLoginForm.value.userName;
+      loginUser.password = this.userLoginForm.value.password;
       return this.loginService.getLoggedInUser(loginUser)
                .subscribe(
                  loggedInUser => {
                   if(loggedInUser){
-                    this.authService.login(this.form.value);    // {7}
+                    this.authService.login(this.userLoginForm.value);    // {7}
                     this.formSubmitAttempt = true;             // {8}
                   }else{
                     console.log("In else");
