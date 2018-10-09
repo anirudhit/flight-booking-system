a09787cfe01a8d12b8d2fc7524ae7e9815b41a58
anirudhit <anirudhit@outlook.com>
2018-10-08 20:04:40 -0500

    app routing (FE)
    - Updated login component import

--- fbs-front-end/src/app/app-routing.module.ts
+++ fbs-front-end/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard } from './auth/auth.guard';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login/components/login.component';
 import { HomeComponent } from './home/home.component';
 
 // import { CustomerComponent } from './customer/customer.component';
