3eab1a1db89f67bf4bc57d92dfc30c83d02f1ef5
anirudhit <anirudhit@outlook.com>
2018-10-09 12:23:30 -0500

    register component (FE)
    - Added user register form

--- fbs-front-end/src/app/register/register.component.ts
+++ fbs-front-end/src/app/register/register.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  userRegisterForm: FormGroup;
+  private formSubmitAttempt: boolean; // {2}
+  constructor(
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.userRegisterForm = this.fb.group({     // {5}
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  isFieldInvalid(field: string) { // {6}
+    return (
+      (!this.userRegisterForm.get(field).valid && this.userRegisterForm.get(field).touched) ||
+      (this.userRegisterForm.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+
+  onSubmit() {
+    alert("Register");
+  }
 }
