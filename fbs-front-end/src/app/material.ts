cdf10cacc6f8abba7e828e7b6dae963be6061822
anirudhit <anirudhit@outlook.com>
2018-10-11 16:28:02 -0500

    material module (FE)
    - Formated the modules text

--- fbs-front-end/src/app/material.ts
+++ fbs-front-end/src/app/material.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSelectModule} from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+
 @NgModule({
     imports: [
         MatFormFieldModule,
