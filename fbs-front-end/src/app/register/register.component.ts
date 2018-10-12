0f6aaa54295dbbe94bb788d184c2d08634be067a
anirudhit <anirudhit@outlook.com>
2018-10-12 00:12:24 -0500

    register component (FE)
    - Added register functionality

--- fbs-front-end/src/app/register/register.component.ts
+++ fbs-front-end/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RegisterService } from './services/register.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +8,13 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   userRegisterForm: FormGroup;
+  errorMessage: string;
+  showErrorMessage: boolean = false;
+  showHintMessage: boolean = false;
   private formSubmitAttempt: boolean; // {2}
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private registerService: RegisterService
   ) {}
 
   ngOnInit() {
@@ -26,7 +31,58 @@ export class RegisterComponent implements OnInit {
     );
   }
 
-  onSubmit() {
-    alert("Register");
+  checkUsername() {
+    this.showHintMessage = false;
+    this.showErrorMessage = false;
+    this.errorMessage = "";
+    if (this.userRegisterForm.valid) {
+      this.registerService.checkUserRegistrationDetails(this.userRegisterForm.value.userName)
+      .subscribe(isUserRegistered =>{
+          if(isUserRegistered){
+            // Showing exception message if user is registered
+            this.showErrorMessage = true;
+            this.errorMessage = "The username is taken.Try another.";
+          }else{
+            // Register user takes place here
+            this.registerService.getRegisterUserDetails(this.userRegisterForm.value.userName)
+            .subscribe(data => {
+              if(data){
+                // Return valid github user details
+                this.registerUser(data);
+              }else{
+                //Exception message for the invalid github user
+                this.showErrorMessage = true;
+                this.errorMessage = "The username is not available in github.Try another.";
+              }
+            },error =>{
+              if(error){
+                //Exception message for the invalid github user
+                this.showErrorMessage = true;
+                this.errorMessage = "The username is not available in github.Try another.";
+              }
+            });
+          }
+      });
+    }
+  }
+
+  registerUser(userData : any){
+    let userObj = {
+      username: userData.login,
+      imageUrl: userData.avatar_url,
+      name: userData.name,
+      email: userData.email,
+      password: this.userRegisterForm.value.password
+    };
+    this.registerService.createUserRegistration(userObj)
+    .subscribe(isUserRegistered =>{
+        if(isUserRegistered){
+          // Showing the hint message
+          this.showHintMessage = true;
+          this.userRegisterForm.get('userName').patchValue('');
+          this.userRegisterForm.get('password').patchValue('');
+          this.userRegisterForm.reset();
+        }
+    });
   }
 }
