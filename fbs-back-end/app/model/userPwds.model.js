38bc3bfd90eb72da796fc418f411cf84b688c05f
anirudhit <anirudhit@outlook.com>
2018-10-12 00:14:16 -0500

    userPwds model (BE)
    - Updated user password model

--- fbs-back-end/app/model/userPwds.model.js
+++ fbs-back-end/app/model/userPwds.model.js
@@ -1,10 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
 	const UserPwd = sequelize.define('userPwd', {
-	  id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true
-	  },
-	  user_id: {
+	  userid: {
 			type: Sequelize.STRING
 	  },
 	  password: {
