6f165403f86c8fbcecdc79d45ace5e5622761722
anirudhit <anirudhit@outlook.com>
2018-10-14 21:42:06 -0500

    angular material (FE)
    - Added MatMenuModule, MatIconModule

--- fbs-front-end/src/app/app-material/app-material.module.ts
+++ fbs-front-end/src/app/app-material/app-material.module.ts
@@ -5,6 +5,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
@@ -12,14 +14,18 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
 ],
   exports: [
     MatToolbarModule,
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ]
 })
 export class AppMaterialModule {}
