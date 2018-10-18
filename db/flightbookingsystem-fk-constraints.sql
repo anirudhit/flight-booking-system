14d6f3caa0d3a11779706151d2b0097349ced61f
anirudhit <anirudhit@outlook.com>
2018-10-18 17:16:31 -0500

    flightbookingsystem fk constraints
    - Added contraints file

---
+++ db/flightbookingsystem-fk-constraints.sql
@@ -0,0 +1,14 @@
+-- Adding constraints
+-- (t_user_pwds)
+ALTER TABLE t_user_pwds ADD CONSTRAINT t_user_pwds_user_id_fk
+FOREIGN KEY (user_id) REFERENCES t_users(id);
+
+-- (t_flight_schedule)
+ALTER TABLE t_flight_schedule ADD CONSTRAINT t_flight_schedule_arrival_id_fk
+FOREIGN KEY (arrival_id) REFERENCES t_airports(id);
+
+ALTER TABLE t_flight_schedule ADD CONSTRAINT t_flight_schedule_departure_id_fk
+FOREIGN KEY (departure_id) REFERENCES t_airports(id);
+
+ALTER TABLE t_flight_schedule ADD CONSTRAINT t_flight_schedule_flight_id_fk
+FOREIGN KEY (flight_id) REFERENCES t_flights(id);
