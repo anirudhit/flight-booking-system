c341176752a75d8c6fb2f0768239c19e86618183
anirudhit <anirudhit@outlook.com>
2018-10-18 17:14:04 -0500

    flightSchedule model (BE)
    - Created flights schedule model

---
+++ fbs-back-end/app/model/flightSchedule.model.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, Sequelize) => {
+	const FlightSchedule = sequelize.define('t_flight_schedules', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
+        arrival_id: {
+            type: Sequelize.INTEGER
+        },
+        departure_id: {
+            type: Sequelize.INTEGER
+        },
+        arrival_time: {
+            type: Sequelize.STRING
+        },
+        departure_time: {
+            type: Sequelize.STRING
+        },
+        fare: {
+            type: Sequelize.STRING
+        },
+        duration: {
+            type: Sequelize.STRING
+        },
+        flight_id: {
+            type: Sequelize.INTEGER
+        }
+	});
+	return FlightSchedule;
+}
