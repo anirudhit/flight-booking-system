0b93b17cf557019e7b7d5d50a708a5b9bf46df35
anirudhit <anirudhit@outlook.com>
2018-10-18 00:32:39 -0500

    airport route (BE)
    - Added route for airport arrival list
    - Added route for airport departure list

--- fbs-back-end/app/route/airport.route.js
+++ fbs-back-end/app/route/airport.route.js
@@ -3,6 +3,8 @@ module.exports = function(app) {
     const airports = require('../controller/airport.controller.js');
  
     // Airport list
-    app.get('/api/airports/list', airports.airportsList)
+    app.get('/api/airports/list', airports.airportsList);
+    app.get('/api/airports/arrival/list', airports.airportsArrivalList);
+    app.get('/api/airports/departure/list', airports.airportsDepartureList);
     
 }
