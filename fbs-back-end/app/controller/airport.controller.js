3bea7980049bc10c3dca5358d6ac89d6e9630c05
anirudhit <anirudhit@outlook.com>
2018-10-18 00:33:15 -0500

    airport controller (BE)
    - Added method for airport arrival list
    - Added method for airport departure list

--- fbs-back-end/app/controller/airport.controller.js
+++ fbs-back-end/app/controller/airport.controller.js
@@ -1,11 +1,35 @@
 const db = require('../config/db.config.js');
 const Airport = db.t_airports;
-
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 //Send all Airports list
 exports.airportsList = (req, res) => {
     Airport.findAll({
         attributes: ['id','name','code','city','country_code']
+      }).then(airports => {
+        res.json(airports);
+    });
+};
+
+//Send arrival Airports list
+exports.airportsArrivalList = (req, res) => {
+    Airport.findAll({
+        attributes: ['id','name','code','city','country_code']
+      }).then(airports => {
+        res.json(airports);
+    });
+};
+
+//Send departure Airports list
+exports.airportsDepartureList = (req, res) => {
+    Airport.findAll({
+        attributes: ['id','name','code','city','country_code'],
+        where: {
+            id: {
+              [Op.ne]: [1]
+            }
+        }
       }).then(airports => {
         res.json(airports);
     });
