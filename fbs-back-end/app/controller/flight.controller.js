a3c051f0f3a2375608da5570c731cf88c40ce49b
anirudhit <anirudhit@outlook.com>
2018-10-18 17:14:58 -0500

    flight controller (BE)
    - Added fcreateFlightSchedule logic

--- fbs-back-end/app/controller/flight.controller.js
+++ fbs-back-end/app/controller/flight.controller.js
@@ -1,12 +1,21 @@
 const db = require('../config/db.config.js');
 const Flight = db.t_flights;
+const FlightSchedule = db.t_flight_schedules;
 
-
-//Send all Airports list
+//Send all Flights list
 exports.flightsList = (req, res) => {
     Flight.findAll({
         attributes: ['id','code','name']
       }).then(flights => {
         res.json(flights);
     });
+};
+
+//Create a Flight schedule
+exports.createFlightSchedule = (req, res) => {
+    let flightSchedule = req.body;
+    console.log(flightSchedule);
+    FlightSchedule.create(flightSchedule).then(result => {
+		res.json(result);
+	});
 };
