bbb14b32063a5c8fe1fe919a124bb0077805324f
anirudhit <anirudhit@outlook.com>
2018-10-18 17:13:32 -0500

    flight route (BE)
    - Added api route for flight schedule

--- fbs-back-end/app/route/flight.route.js
+++ fbs-back-end/app/route/flight.route.js
@@ -3,6 +3,7 @@ module.exports = function(app) {
     const flights = require('../controller/flight.controller.js');
  
     // Airport list
-    app.get('/api/flights/list', flights.flightsList)
+    app.get('/api/flights/list', flights.flightsList);
+    app.post('/api/flights/schedule', flights.createFlightSchedule);
     
 }
