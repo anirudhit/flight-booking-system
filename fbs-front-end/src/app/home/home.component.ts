aef7d7a5fc9b1227e6bf53c86925f678030a7b9d
anirudhit <anirudhit@outlook.com>
2018-10-21 17:49:58 -0500

    home component (FE)
    - Added tiles in the home page for navigation

--- fbs-front-end/src/app/home/home.component.ts
+++ fbs-front-end/src/app/home/home.component.ts
@@ -2,14 +2,43 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template:'<p>You are logged in!</p>',
-  styles: []
+  templateUrl:'./home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    this.initMenu();
+  }
+
+  centered = false;
+  disabled = false;
+  unbounded = false;
+
+  radius: number = 40;
+  color: string;
+
+  menuOptions : any[] = [];
+
+  initMenu(){
+    this.menuOptions.push({
+      name: "Profile",
+      page: "/profile"
+    });
+    this.menuOptions.push({
+      name: "Book a ticket",
+      page: "/profile"
+    });
+    this.menuOptions.push({
+      name: "Booking history",
+      page: "/profile"
+    });
+    this.menuOptions.push({
+      name: "Schedule flight",
+      page: "/flightschedule"
+    });
   }
 
 }
