22a4aba34881eee8c344d73f4dbd8256f48e1a80
anirudhit <anirudhit@outlook.com>
2018-10-21 03:12:34 -0500

    user model (BE)
    - Updated user model for the profile view and update

--- fbs-back-end/app/model/user.model.js
+++ fbs-back-end/app/model/user.model.js
@@ -6,15 +6,33 @@ module.exports = (sequelize, Sequelize) => {
 	  },
 	  username: {
 			type: Sequelize.STRING
+		},
+		name: {
+			type: Sequelize.STRING
+		},
+		first_name: {
+			type: Sequelize.STRING
+		},
+		last_name: {
+			type: Sequelize.STRING
+		},
+		middle_name: {
+			type: Sequelize.STRING
+		},
+		ssn: {
+			type: Sequelize.STRING
+		},
+		cell_number: {
+			type: Sequelize.STRING
 	  },
 	  image_url: {
 		  type: Sequelize.STRING
     },
-    name: {
-      type: Sequelize.STRING
-    },
     email: {
       type: Sequelize.STRING
+		},
+		address: {
+		  type: Sequelize.STRING
     }
 	});
 	return User;
