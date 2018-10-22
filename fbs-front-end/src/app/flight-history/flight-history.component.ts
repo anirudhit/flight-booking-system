7da4ae6626e08360285e79c619864bfb27accd60
anirudhit <anirudhit@outlook.com>
2018-10-21 21:22:19 -0500

    flight history component (FE)
    - Added flight history component

---
+++ fbs-front-end/src/app/flight-history/flight-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-history',
+  templateUrl: './flight-history.component.html',
+  styleUrls: ['./flight-history.component.css']
+})
+export class FlightHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
