b24078244f5ce605eb0d0e301bd692b8823b6ebc
anirudhit <anirudhit@outlook.com>
2018-10-27 21:51:59 -0500

    flight controller (BE)
    - Added getFlightSchedules call

--- fbs-back-end/app/controller/flight.controller.js
+++ fbs-back-end/app/controller/flight.controller.js
@@ -1,6 +1,10 @@
 const db = require('../config/db.config.js');
 const Flight = db.t_flights;
 const FlightSchedule = db.t_flight_schedules;
+const Airport = db.t_airports;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const flightQueries = require('../queries/flights');
 
 //Send all Flights list
 exports.flightsList = (req, res) => {
@@ -17,4 +21,77 @@ exports.createFlightSchedule = (req, res) => {
     FlightSchedule.create(flightSchedule).then(result => {
 		res.json(result);
 	});
+};
+
+//Get flight schedule list
+exports.getFlightSchedules = (req, res) => {
+    FlightSchedule.sequelize.query(flightQueries.FLIGHT_SCHEDULE_LIST,{ type: Sequelize.QueryTypes.SELECT})
+    .then(scheduleList => {
+            res.json(scheduleList);
+    });
+    let flightSchedule = req.query;
+    /*FlightSchedule.hasOne(Airport, {
+        foreignKey: 'arrival_id',
+        sourceKey: 'id',
+        constraints: false
+    });
+    FlightSchedule.belongsTo(Airport, {as: 'arrival_airport', foreignKey: 'arrival_id', sourceKey: 'id'});
+
+    FlightSchedule.hasOne(Airport, {
+        foreignKey: 'departure_id',
+        sourceKey: 'id',
+        constraints: false
+    });
+    FlightSchedule.belongsTo(Airport, {as: 'departure_airport', foreignKey: 'departure_id', sourceKey: 'id'});
+
+    FlightSchedule.hasOne(Flight, {
+        foreignKey: 'flight_id',
+        sourceKey: 'id',
+        constraints: false
+    });
+    FlightSchedule.belongsTo(Flight, {as: 'flight', foreignKey: 'flight_id', sourceKey: 'id'});
+
+    FlightSchedule.findAll({
+        attributes: ['id','arrival_time','departure_time','arrival_terminal',
+                        'departure_terminal', 'fare', 'duration', 'createdAt', 'updatedAt'],
+        include: [
+        {
+            model: Airport,
+            as: 'arrival_airport',
+            attributes: ['id', 'name', 'code', 'country_code', 'city']
+        },
+        {
+            model: Airport,
+            as: 'departure_airport',
+            attributes: ['id', 'name', 'code', 'country_code', 'city']
+        },
+        {
+            model: Flight,
+            as: 'flight',
+            attributes: ['id', 'name', 'code']
+        }
+    ]
+    }).then(schedules => {
+        // const resObj = schedules.map(schedule => {
+        //     //console.log(schedule);
+        //     return Object.assign(
+        //         {},
+        //         {
+        //           id: schedule.id,
+        //           arrival_time: schedule.arrival_time,
+        //           departure_time: schedule.departure_time,
+        //           arrival_terminal: schedule.arrival_terminal,
+        //           departure_terminal: schedule.departure_terminal,
+        //           arrival_airport: schedule.t_airport,
+        //           flight_info: schedule.t_flight,
+        //           fare: schedule.fare,
+        //           duration: schedule.duration,
+        //           createdAt: schedule.createdAt,
+        //           updatedAt: schedule.updatedAt
+        //         }
+        //       )
+        // });
+        // res.json(resObj);
+        //res.json(schedules);
+    });*/
 };
