e07a6c746915462649ee764b28c06b50382553d2
anirudhit <anirudhit@outlook.com>
2018-10-28 20:55:01 -0500

    flight history component (FE)
    - Added depenencies with update dialog

--- fbs-front-end/src/app/flight-history/flight-history.component.ts
+++ fbs-front-end/src/app/flight-history/flight-history.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FlightHistoryService } from './services/flight-history.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 export interface DeleteScheduleData {
   schedule: any
+}
+
+export interface UpdateScheduleData {
+  id: number,
+  departure_terminal: string,
+  arrival_terminal: string,
+  departure_time: string,
+  arrival_time: string,
+  fare: number
 }
 
 @Component({
@@ -28,6 +38,23 @@ export class FlightHistoryComponent implements OnInit {
     this.flightHistoryService.getFlightSchedulesList()
     .subscribe(schedules => {
         this.flightSchedules = schedules;
+    });
+  }
+
+  updateSchedule(schedule){
+    console.log(schedule);
+  }
+
+  openUpdateScheduleDialog(schedule): void {
+    const dialogRef = this.dialog.open(UpdateScheduleDialog, {
+      width: '550px',
+      data: schedule
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result){
+        this.updateSchedule(result);
+      }
     });
   }
 
@@ -77,6 +104,48 @@ export class DeleteScheduleDialog {
   
   onDeleteClick(): void {
     this.dialogRef.close(this.data);
+  }
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+}
+
+@Component({
+  selector: 'update-schedule-dialog',
+  templateUrl: 'dialog/update-schedule-dialog.html',
+})
+export class UpdateScheduleDialog {
+
+  updateFlightScheduleForm : FormGroup;
+
+  constructor(
+    private fb      : FormBuilder,
+    public dialogRef: MatDialogRef<UpdateScheduleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: UpdateScheduleData
+  ) {
+    this.createAdminFlightScheduleForm();
+  }
+
+  createAdminFlightScheduleForm() {
+    this.updateFlightScheduleForm  = this.fb.group({
+      departure_time     : [this.data.departure_time, Validators.required],
+      arrival_time       : [this.data.arrival_time, Validators.required],
+      departure_terminal : [this.data.departure_terminal, Validators.required],
+      arrival_terminal   : [this.data.arrival_terminal, Validators.required],
+      fare               : [this.data.fare, Validators.required]
+    });
+  }
+  
+  onUpdateClick(): void {
+    let scheduleObj = {
+      id: this.data.id,
+      value: this.updateFlightScheduleForm.value
+    };
+    if(this.updateFlightScheduleForm.valid){
+      this.dialogRef.close(scheduleObj);
+    }
   }
 
   onCancelClick(): void {
