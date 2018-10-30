ea3fba80c18b333559628c70758a3b92bc5406ba
anirudhit <anirudhit@outlook.com>
2018-10-30 02:21:24 -0500

    flight route (BE)
    - Added route for the  flight schedule update

--- fbs-back-end/app/route/flight.route.js
+++ fbs-back-end/app/route/flight.route.js
@@ -3,10 +3,10 @@ module.exports = function(app) {
     const flights = require('../controller/flight.controller.js');
  
     // Airport list
-    app.get('/api/flights/list', flights.flightsList);
-    app.post('/api/flights/schedule', flights.createFlightSchedule);
-    app.get('/api/flights/schedule/list', flights.getFlightSchedules);
-    app.delete('/api/flights/schedule/:flightId', flights.deleteFlightSchedule);
-    
+    app.get('/api/flights/list', flights.flightsList);// Get the flight list
+    app.post('/api/flights/schedule', flights.createFlightSchedule);// Create the flight schedule
+    app.get('/api/flights/schedule/list', flights.getFlightSchedules);// Get the flight schedule list
+    app.delete('/api/flights/schedule/:flightId', flights.deleteFlightSchedule);// Delete a flight schedule
+    app.put('/api/flights/schedule', flights.updateFlightSchedule);// Update a flight schedule
     
 }
