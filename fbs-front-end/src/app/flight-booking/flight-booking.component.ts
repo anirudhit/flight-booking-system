880b761277c436fa72b430089e20c73438764590
anirudhit <anirudhit@outlook.com>
2018-10-30 17:01:40 -0500

    flight-booking component
    - Added horizontal stepper form

--- fbs-front-end/src/app/flight-booking/flight-booking.component.ts
+++ fbs-front-end/src/app/flight-booking/flight-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-flight-booking',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flight-booking.component.css']
 })
 export class FlightBookingComponent implements OnInit {
-
-  constructor() { }
+  isLinear = true;
+  planTravelFormGroup: FormGroup;
+  flightSelectionFormGroup: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.planTravelFormGroup = this.fb.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.flightSelectionFormGroup = this.fb.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
 
 }
