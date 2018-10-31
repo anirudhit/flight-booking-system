8cd3a5325387c704db7d6bee7b15d0b759d118cf
anirudhit <anirudhit@outlook.com>
2018-10-31 11:09:26 -0500

    flight-booking component (FE)
    - Added design for the travel plan
    - Added services for the arrivals and departures
    - Added date picker for the depart date

--- fbs-front-end/src/app/flight-booking/flight-booking.component.ts
+++ fbs-front-end/src/app/flight-booking/flight-booking.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {  FlightBookingService } from './services/flight-booking.service';
 
 @Component({
   selector: 'app-flight-booking',
@@ -8,19 +9,61 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class FlightBookingComponent implements OnInit {
   isLinear = true;
-  planTravelFormGroup: FormGroup;
-  flightSelectionFormGroup: FormGroup;
+  planTravelFormGroup       : FormGroup;
+  flightSelectionFormGroup  : FormGroup;
+  arrivalAirportList        : any;
+  departureAirportList      : any;
+  departMinDate             : Date = new Date();
+  departMaxDate             : Date;
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private flightBookingService: FlightBookingService
   ) {}
 
   ngOnInit() {
+    this.initFlightBookingForm();
+    this.loadAirportsDepartureList();
+  }
+
+  initFlightBookingForm(){
+    let dayMs           : number = 24*60*60*1000;
+    let nextThreeMonths : number = dayMs*90;
+    let todayTs         : number = new Date().getTime();
+    let nextThreeMonthsTs : number = todayTs + nextThreeMonths;
+    this.departMaxDate  = new Date(nextThreeMonthsTs);
     this.planTravelFormGroup = this.fb.group({
-      firstCtrl: ['', Validators.required]
+      departure         : ['', Validators.required],
+      arrival           : [{ value: '', disabled:true }, Validators.required],
+      departDate        : [null, Validators.required]
     });
     this.flightSelectionFormGroup = this.fb.group({
       secondCtrl: ['', Validators.required]
     });
+  }
+
+  loadAirportsDepartureList(){
+    this.flightBookingService.getAirportsDepartureList()
+    .subscribe(airports => {
+        this.departureAirportList = airports;
+    });
+  }
+
+  loadAirportsArrivalList(arrivalId){
+    this.flightBookingService.getAirportsArrivalList(arrivalId)
+    .subscribe(airports => {
+        this.arrivalAirportList = airports;
+    });
+  }
+
+  selectArrival(arrivalObj){
+    //this.arrival_id = arrivalObj.id;
+  }
+
+  selectDeparture(departureObj){
+    this.planTravelFormGroup.get('arrival').patchValue("");
+    this.planTravelFormGroup.get('arrival').enable();
+    //this.departure_id = departureObj.id;
+    this.loadAirportsArrivalList(departureObj.id);
   }
 
 }
--- fbs-front-end/src/app/flight-booking/services/flight-booking.service.ts
+++ fbs-front-end/src/app/flight-booking/services/flight-booking.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
 export class FlightBookingService {
+  private airportsUrl = '/api/airports';
+  private flightsUrl = '/api/flights';
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  getAirportsDepartureList(){
+    let url = this.airportsUrl + '/departure/schedule/list';
+    return this.http.get(url);
+  }
+
+  getAirportsArrivalList(arrivalId){
+    let url = this.airportsUrl + '/arrival/schedule/list?id='+arrivalId;
+    return this.http.get(url);
+  }
 }
