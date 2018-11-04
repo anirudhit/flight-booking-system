5c17f523101a6edae6a993539c8bc6967d66fbae
anirudhit <anirudhit@outlook.com>
2018-11-04 01:52:33 -0600

    header-fb component (FE)
    - Created header-fb component

---
+++ fbs-front-end/src/app/header-fb/header-fb.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth-fb/auth.service';
+import { FirebaseUserModel } from '../auth-fb/user.model';
+@Component({
+  selector: 'app-header-fb',
+  templateUrl: './header-fb.component.html',
+  styleUrls: ['./header-fb.component.css']
+})
+export class HeaderFbComponent implements OnInit {
+  user: FirebaseUserModel = new FirebaseUserModel();
+  constructor(
+    public authService: AuthService,
+    private route: ActivatedRoute,
+    private location : Location
+  ) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(routeData => {
+      let data = routeData['data'];
+      if (data) {
+        this.user = data;
+        console.log(this.user);
+      }
+    })
+  }
+
+  onLogout() {
+    this.authService.doLogout()
+    .then((res) => {
+      this.location.back();
+    }, (error) => {
+      console.log("Logout error", error);
+    });
+  }
+
+}
