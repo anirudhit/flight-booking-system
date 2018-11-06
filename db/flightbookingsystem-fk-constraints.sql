77debe290258e72d73cefe3543d8c48bf6e4b7aa
anirudhit <anirudhit@outlook.com>
2018-11-06 15:08:46 -0600

    flightbookingsystem-fk-constraints (db)
    - Removed constraints for user_roles, user_password table

--- db/flightbookingsystem-fk-constraints.sql
+++ db/flightbookingsystem-fk-constraints.sql
@@ -1,12 +1,12 @@
 -- Adding constraints
 
 -- (t_users)
-ALTER TABLE t_users ADD CONSTRAINT t_users_user_role_id_fk
-FOREIGN KEY (role_id) REFERENCES t_user_roles(id);
+--ALTER TABLE t_users ADD CONSTRAINT t_users_user_role_id_fk
+--FOREIGN KEY (role_id) REFERENCES t_user_roles(id);
 
 -- (t_user_pwds)
-ALTER TABLE t_user_pwds ADD CONSTRAINT t_user_pwds_user_id_fk
-FOREIGN KEY (user_id) REFERENCES t_users(id);
+--ALTER TABLE t_user_pwds ADD CONSTRAINT t_user_pwds_user_id_fk
+--FOREIGN KEY (user_id) REFERENCES t_users(id);
 
 -- (t_flight_schedule)
 ALTER TABLE t_flight_schedule ADD CONSTRAINT t_flight_schedule_arrival_id_fk
