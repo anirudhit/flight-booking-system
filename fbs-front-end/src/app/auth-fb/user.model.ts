25ccf8a65cec8ab36184fe4fad2f80ecdb4da912
anirudhit <anirudhit@outlook.com>
2018-11-05 19:41:45 -0600

    user modal (FE)
    - Added id, email to user object

--- fbs-front-end/src/app/auth-fb/user.model.ts
+++ fbs-front-end/src/app/auth-fb/user.model.ts
@@ -1,11 +1,15 @@
 export class FirebaseUserModel {
+  id: string;
   image: string;
   name: string;
+  email: string;
   provider: string;
 
   constructor(){
+    this.id = "";
     this.image = "";
     this.name = "";
+    this.email = "";
     this.provider = "";
   }
 }
