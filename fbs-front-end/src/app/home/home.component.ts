d06b562a7b53f2a4d010656eac63f8c68aee3840
anirudhit <anirudhit@outlook.com>
2018-11-06 22:06:56 -0600

    home component (FE)
    - Added initUserDetails
    - Added Usermenu and Adminmenu

--- fbs-front-end/src/app/home/home.component.ts
+++ fbs-front-end/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { FirebaseUserModel } from '../auth-fb/user.model';
+
 @Component({
   selector: 'app-home',
   templateUrl:'./home.component.html',
@@ -7,12 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this.initMenu();
+    this.menuOptions = [];
+    this.initUserDetails();
   }
 
+  user: FirebaseUserModel = new FirebaseUserModel();
   centered = false;
   disabled = false;
   unbounded = false;
@@ -22,7 +29,36 @@ export class HomeComponent implements OnInit {
 
   menuOptions : any[] = [];
 
-  initMenu(){
+  initUserDetails(){
+    this.route.data.subscribe(routeData => {
+      let data = routeData['data'];
+      if (data) {
+        this.user = data;
+        if(this.user.isAdmin){
+          this.initAdminMenu();
+        }else{
+          this.initUserMenu();
+        }
+      }
+    })
+  }
+
+  initUserMenu(){
+    this.menuOptions.push({
+      name: "Book a ticket",
+      page: "/flightsbooking"
+    });
+    this.menuOptions.push({
+      name: "Booking history",
+      page: "/profile"
+    });
+    this.menuOptions.push({
+      name: "Profile",
+      page: "/profile"
+    });
+  }
+
+  initAdminMenu(){
     this.menuOptions.push({
       name: "Book a ticket",
       page: "/flightsbooking"
