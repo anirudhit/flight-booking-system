c7061b258dbf15939e61772c42bc6f9a1679ec02
anirudhit <anirudhit@outlook.com>
2018-11-10 03:31:31 -0600

    my-trips component (FE)
    - Added upcoming, cancelled and history trips

--- fbs-front-end/src/app/my-trips/my-trips.component.ts
+++ fbs-front-end/src/app/my-trips/my-trips.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MyTripsService } from './services/my-trips.service';
+
+import { ActivatedRoute } from '@angular/router';
+import { FirebaseUserModel } from '../auth-fb/user.model';
 
 @Component({
   selector: 'app-my-trips',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyTripsComponent implements OnInit {
 
-  constructor() { }
+  user: FirebaseUserModel = new FirebaseUserModel();
+  tripIndex : number = 0;
+  myUpcomingTripsList : any;
+  myTripsLoading : boolean = true;
+  upcomingTripsLoading : boolean = true;
+  cancelledTripsLoading : boolean = true;
+  historyTripsLoading : boolean = true;
+  constructor(
+    private myTripsService:  MyTripsService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.initUserDetails();
+  }
+
+  initUserDetails(){
+    this.myTripsLoading = true;
+    this.route.data.subscribe(routeData => {
+      let data = routeData['data'];
+      if (data) {
+        this.user = data;
+        this.myTripsLoading = false;
+        this.loadUpcomingTripsList();
+      }
+    })
+  }
+
+  loadUpcomingTripsList(){
+    this.upcomingTripsLoading = true;
+    this.myTripsService.getUpcomingTripsList(this.user.id)
+    .subscribe(upcomingTripsList => {
+        this.myUpcomingTripsList = upcomingTripsList;
+        this.upcomingTripsLoading = false;
+    });
+    
+  }
+
+  selectedTabIndex(loadingTabIndex){
+    if(loadingTabIndex === 0){
+      this.loadUpcomingTripsList();
+    }
   }
 
 }
