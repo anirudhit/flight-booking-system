28f8d2b83ed2e6faecc8081b51e05885ecc1e95f
anirudhit <anirudhit@outlook.com>
2018-11-10 03:34:08 -0600

    bookingg route (BE)
    - Added route for upcoming trips

--- fbs-back-end/app/route/booking.route.js
+++ fbs-back-end/app/route/booking.route.js
@@ -5,5 +5,7 @@ module.exports = function(app) {
     //app.get('/api/booking', user.userDetails);// Get the booking details
     app.post('/api/booking', bookings.bookATicket);// Create a booking
     //app.put('/api/booking', user.updateUserDetails);// Update a booking
-    
+
+
+    app.get('/api/booking/trips/upcoming', bookings.userUpcomingTrips);// Get user trips
 }
