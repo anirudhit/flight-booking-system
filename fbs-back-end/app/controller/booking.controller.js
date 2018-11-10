c0bb4d16ef4323781c30edd39f6fceecd7ad8cc5
anirudhit <anirudhit@outlook.com>
2018-11-10 03:35:18 -0600

    booking controller (BE)
    - Added method for userUpcomingTrips

--- fbs-back-end/app/controller/booking.controller.js
+++ fbs-back-end/app/controller/booking.controller.js
@@ -2,6 +2,10 @@ const db = require('../config/db.config.js');
 const uniqid = require('uniqid');
 const FlightBooking = db.t_flight_bookings;
 const Passenger = db.t_passengers;
+const FlightSchedule = db.t_flight_schedules;
+const Airport = db.t_airports;
+const bookingQueries = require('../queries/booking');
+const Sequelize = require('sequelize');
 
 exports.bookATicket = (req, res) => {
     let registerUserReq = req.body;
@@ -33,4 +37,59 @@ exports.bookATicket = (req, res) => {
     //     //console.log(result);
     //     res.send("Done!");
     // });
+};
+
+
+exports.userUpcomingTrips = (req, res) => {
+    let userId = req.query.userId;
+    let query = bookingQueries.BOOKING_MY_TRIPS + '\''+userId+ '\'';
+    console.log(userId);
+    FlightBooking.sequelize.query(query,{ type: Sequelize.QueryTypes.SELECT})
+    .then(ticketList => {
+        res.json(ticketList);
+    });
+    // FlightBooking.hasOne(FlightSchedule, {
+    //     foreignKey: 'flight_schedule_id',
+    //     sourceKey: 'id',
+    //     constraints: false
+    // });
+    // FlightBooking.belongsTo(FlightSchedule, {foreignKey: 'flight_schedule_id', sourceKey: 'id'});
+
+    // FlightSchedule.hasOne(Airport, {
+    //     foreignKey: 'arrival_id',
+    //     sourceKey: 'id',
+    //     constraints: false
+    // });
+    // FlightSchedule.belongsTo(Airport, {as: 'arrival_airport', foreignKey: 'arrival_id', sourceKey: 'id'});
+    
+    // FlightSchedule.hasOne(Airport, {
+    //     foreignKey: 'departure_id',
+    //     sourceKey: 'id',
+    //     constraints: false
+    // });
+    // FlightSchedule.belongsTo(Airport, {as: 'departure_airport', foreignKey: 'departure_id', sourceKey: 'id'});
+    
+    
+    // FlightBooking.findAll({
+    //     where: {user_id: userId},
+    //     attributes: ['id','flight_schedule_id','user_id','date_of_journey',
+    //                     'payment_id', 'email', 'cancel_booking', 'createdAt'],
+    //     include: [{
+    //         model: FlightSchedule,
+    //         attributes: ['id', 'departure_id', 'arrival_id'],
+    //         include:[{
+    //             model: Airport,
+    //             as: 'arrival_airport',
+    //             attributes: ['id', 'name', 'code', 'country_code', 'city']
+    //         },
+    //         {
+    //             model: Airport,
+    //             as: 'departure_airport',
+    //             attributes: ['id', 'name', 'code', 'country_code', 'city']
+    //         }]
+    //     }]
+    // }).then(tickets => {
+    //     res.json(tickets);
+    // });
+    
 };
