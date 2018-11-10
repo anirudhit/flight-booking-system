aaaa94a470720cd7438d299059cbdd3163693a31
anirudhit <anirudhit@outlook.com>
2018-11-10 03:32:48 -0600

    mt-trips service (FE)
    - Added getUpcomingTripsList method

--- fbs-front-end/src/app/my-trips/services/my-trips.service.ts
+++ fbs-front-end/src/app/my-trips/services/my-trips.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
 export class MyTripsService {
+  private flightBookingUrl = '/api/booking/trips';
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  getUpcomingTripsList(userId){
+    let url = this.flightBookingUrl+ '/upcoming' +'?userId='+userId;
+    return this.http.get(url);
+  }
 }
