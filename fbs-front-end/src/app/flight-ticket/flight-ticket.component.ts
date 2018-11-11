57856646fb8f53bb8510e8f62b0d09637afb1d18
anirudhit <anirudhit@outlook.com>
2018-11-11 15:27:40 -0600

    flight-ticket component (FE)
    - Added flight-ticket component

---
+++ fbs-front-end/src/app/flight-ticket/flight-ticket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-ticket',
+  templateUrl: './flight-ticket.component.html',
+  styleUrls: ['./flight-ticket.component.css']
+})
+export class FlightTicketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
